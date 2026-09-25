82ff599f157c9ebb1fe3505e5e232e93ab6096e1
atampy25 <24306974+atampy25@users.noreply.github.com>
2026-09-25 12:29:57 +0200

    Mod template

---
+++ .github/validate.mjs
@@ -0,0 +1,17 @@
+import child_process from "node:child_process";
+
+const output = JSON.parse(
+	child_process.execFileSync(".github/Simple Mod Framework", [
+		"validate-mod",
+		"--lenient",
+		".",
+	]),
+);
+
+if (output.result === "pass") {
+	console.log("Validation passed");
+} else {
+	console.log("Validation failed");
+	console.log(output);
+	process.exit(1);
+}
